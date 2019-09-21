32b8fef887eb25db878c3cd8b83533b8d0a443f0
takahiko mori <tarhiko@gmail.com>
2019-09-21 20:05:52 +0900

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,21 @@
 
 void setup() {
   // put your setup code here, to run once:
+  //pinMode(LED_BUILTIN, OUTPUT);
+  //  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  /*
+    for (int i=0; i<10; i++) {
+        digitalWrite(LED_BUILTIN, HIGH);
+        Serial.println("High");
+        delay(1000);
+        digitalWrite(LED_BUILTIN, LOW);
+        Serial.println("LOW");
+        delay(1000);
+    }
+    delay(500);
+  */
 }
--- test/test_embedded/test_calculator.cpp
+++ test/test_embedded/test_calculator.cpp
@@ -1,9 +1,9 @@
 #include <Arduino.h>
+#include <unity.h>
 #include "calculator.h"
-#include "unity.h"
 
 
-#ifdef UNIT_TEST
+// #ifdef UNIT_TEST
 
 Calculator calc;
 
@@ -31,20 +31,52 @@ void test_function_calculator_division(void) {
     TEST_ASSERT_EQUAL(33, calc.div(100, 3));
 }
 
+void test_led_builtin_pin_number(void){
+    TEST_ASSERT_EQUAL(13, LED_BUILTIN);
+}
+
+void test_led_state_high(void){
+    digitalWrite(LED_BUILTIN, HIGH);
+    TEST_ASSERT_EQUAL(HIGH, digitalRead(LED_BUILTIN));
+}
+
+void test_led_state_low(void){
+    digitalWrite(LED_BUILTIN, LOW);
+    TEST_ASSERT_EQUAL(LOW, digitalRead(LED_BUILTIN));
+}
+
 void setup() {
+    // Wait for 2 secs 
+    // if board doesn't support softare reset via Serial.DTR/RTS
+    delay(2000);
+   
     UNITY_BEGIN();
+    
+    //check calculate module
     RUN_TEST(test_function_calculator_addition);
     RUN_TEST(test_function_calculator_subtraction);
     RUN_TEST(test_function_calculator_multiplication);
     RUN_TEST(test_function_calculator_division);
-    UNITY_END();
+
+    RUN_TEST(test_led_builtin_pin_number);
+
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(115200);
 }
 
 void loop() {
-    digitalWrite(13, HIGH);
-    delay(100);
-    digitalWrite(13, LOW);
-    delay(500);
+    for (uint8_t i=0; i<10; i++) {
+        //digitalWrite(LED_BUILTIN, HIGH);
+        RUN_TEST(test_led_state_high);
+        Serial.println("High");
+        delay(1000);
+        RUN_TEST(test_led_state_low);
+        //digitalWrite(LED_BUILTIN, LOW);
+        Serial.println("LOW");
+        delay(1000);
+    }
+    UNITY_END();
+
 }
 
-#endif
+// #endif
