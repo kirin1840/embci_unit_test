05fd10b0b051984d4cbc3393ba027efa9a93bec7
takahiko mori <tarhiko@gmail.com>
2019-10-02 03:22:28 +0900

    Upload file

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,41 @@
 #include <Arduino.h>
+#define ENA 5
+#define ENB 6
+#define IN1 7
+#define IN2 8
+#define IN3 9
+#define IN4 11
 
 void setup() {
-  // put your setup code here, to run once:
-  //pinMode(LED_BUILTIN, OUTPUT);
-  //  Serial.begin(115200);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  digitalWrite(ENA, HIGH);  
+  digitalWrite(ENB, HIGH); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  /*
-    for (int i=0; i<10; i++) {
-        digitalWrite(LED_BUILTIN, HIGH);
-        Serial.println("High");
-        delay(1000);
-        digitalWrite(LED_BUILTIN, LOW);
-        Serial.println("LOW");
-        delay(1000);
-    }
-    delay(500);
-  */
+ digitalWrite(IN1, HIGH);      
+  digitalWrite(IN2, LOW); 
+  digitalWrite(IN3, LOW);      
+  digitalWrite(IN4, HIGH);  //go forward
+  delay(500);
+  digitalWrite(IN1, LOW);      
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);      
+  digitalWrite(IN4, LOW);   //stop
+  delay(500);
+  digitalWrite(IN1, LOW);      
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);      
+  digitalWrite(IN4, LOW);   //go back
+  delay(500);
+  digitalWrite(IN1, LOW);      
+  digitalWrite(IN2, LOW); 
+  digitalWrite(IN3, HIGH);      
+  digitalWrite(IN4, HIGH);  //stop
+  delay(500);
 }
--- test/test_embedded/test_calculator.cpp
+++ test/test_embedded/test_calculator.cpp
@@ -2,6 +2,12 @@
 #include <unity.h>
 #include "calculator.h"
 
+#define ENA 5
+#define ENB 6
+#define IN1 7
+#define IN2 8
+#define IN3 9
+#define IN4 11
 
 // #ifdef UNIT_TEST
 
@@ -45,6 +51,49 @@ void test_led_state_low(void){
     TEST_ASSERT_EQUAL(LOW, digitalRead(LED_BUILTIN));
 }
 
+void test_go_forward(void){
+    //  ENB         IN1,4             IN2,3       Description  
+    //  HIGH        HIGH              LOW       Motor is on and turning forwards
+    digitalWrite(IN1, HIGH);
+    TEST_ASSERT_EQUAL(HIGH, digitalRead(IN1));      
+    digitalWrite(IN2, LOW); 
+    TEST_ASSERT_EQUAL(LOW, digitalRead(IN2));
+
+    digitalWrite(IN3, LOW);   
+    TEST_ASSERT_EQUAL(LOW, digitalRead(IN3));   
+    digitalWrite(IN4, HIGH);
+    TEST_ASSERT_EQUAL(HIGH, digitalRead(IN4));
+}
+
+void test_go_back(void){
+    //  ENB         IN1,4           IN2,3       Description  
+    //  HIGH        LOW             HIGH         Motor is on and turning backwards
+    digitalWrite(IN1, LOW);
+    TEST_ASSERT_EQUAL(LOW, digitalRead(IN1));      
+    digitalWrite(IN2, HIGH); 
+    TEST_ASSERT_EQUAL(HIGH, digitalRead(IN2));
+
+    digitalWrite(IN3, HIGH);   
+    TEST_ASSERT_EQUAL(HIGH, digitalRead(IN3));   
+    digitalWrite(IN4, LOW);
+    TEST_ASSERT_EQUAL(LOW, digitalRead(IN4));    
+}
+
+void test_stop(void){
+    //  ENB         IN1,4           IN2,3       Description  
+    //  HIGH        LOW             LOW         Motor is stopped (brakes)
+    digitalWrite(IN1, LOW);
+    TEST_ASSERT_EQUAL(LOW, digitalRead(IN1));      
+    digitalWrite(IN2, LOW); 
+    TEST_ASSERT_EQUAL(LOW, digitalRead(IN2));
+
+    digitalWrite(IN3, LOW);   
+    TEST_ASSERT_EQUAL(LOW, digitalRead(IN3));   
+    digitalWrite(IN4, LOW);
+    TEST_ASSERT_EQUAL(LOW, digitalRead(IN4));
+}
+
+
 void setup() {
     // Wait for 2 secs 
     // if board doesn't support softare reset via Serial.DTR/RTS
@@ -61,22 +110,43 @@ void setup() {
     RUN_TEST(test_led_builtin_pin_number);
 
     pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+    pinMode(ENA, OUTPUT);
+    pinMode(ENB, OUTPUT);
+    digitalWrite(ENA, LOW);  
+    digitalWrite(ENB, LOW); 
+
     Serial.begin(115200);
+
+    digitalWrite(ENA, HIGH);  
+    digitalWrite(ENB, HIGH); 
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
 }
 
 void loop() {
-    for (uint8_t i=0; i<10; i++) {
-        //digitalWrite(LED_BUILTIN, HIGH);
-        RUN_TEST(test_led_state_high);
-        Serial.println("High");
+    for (uint8_t i=0; i<5; i++) {
+        RUN_TEST(test_go_forward);
+        Serial.println("GO Forward");
         delay(1000);
-        RUN_TEST(test_led_state_low);
-        //digitalWrite(LED_BUILTIN, LOW);
-        Serial.println("LOW");
+        RUN_TEST(test_stop);
+        Serial.println("STOP");
+        delay(1000);
+        RUN_TEST(test_go_back);
+        Serial.println("GO BACK");
+        delay(1000);
+        RUN_TEST(test_stop);
+        Serial.println("STOP");        
         delay(1000);
     }
     UNITY_END();
-
+    digitalWrite(ENA, LOW);  
+    digitalWrite(ENB, LOW); 
 }
 
 // #endif
